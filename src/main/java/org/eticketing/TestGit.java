d4e4167c7f32d7c750b9196f7805a709c8375cba
rugambafri <mugishafleuryap@gmail.com>
2021-08-24 17:18:10 +0200

    create my branch

--- src/main/java/org/eticketing/TestGit.java
+++ src/main/java/org/eticketing/TestGit.java
@@ -4,4 +4,8 @@ public class TestGit {
     public  void  Test(){
         System.out.println("c'est un test");
     }
+    public  void Test2(){
+        System.out.println("c'est un test2");
+
+    }
 }
